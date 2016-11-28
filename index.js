91114e5150b9aba3df979b7efd10ac8dcd9d8e9d
ivan <ipkozyrin@gmail.com>
2016-11-28 18:23:15 +1000

    add

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 
+function _schemaRecursuve() {
+
+}
+
+
 function fillDb(mold) {
+  const schema = mold.$$schemaManager.getFullSchema();
+
 
 }
 
 function clearDb(mold) {
-
+  // TODO: заверное надо запросить в драйвере все корни или все докементы и удалить их
 }
 
 export default function (mold) {
