f1c3f040315758e230597ae5783fc4db3bf3b6f2
ivan <ipkozyrin@gmail.com>
2016-11-28 17:20:10 +1000

    add

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import _ from 'lodash';
+
+function fillDb(mold) {
+
+}
+
+function clearDb(mold) {
+
+}
+
+export default function (mold) {
+  return {
+    fillDb: () => fillDb(mold),
+    clearDb: () => clearDb(mold),
+  }
+}
