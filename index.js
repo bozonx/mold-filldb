a35e849b3848d3b7df59c088772ab40acfe8bea5
ivan <ipkozyrin@gmail.com>
2016-11-28 19:37:10 +1000

    change

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Filler {
   constructor(mold) {
     this.mold = mold;
     this.schema = mold.$$schemaManager.getFullSchema();
+    //this.primitivesInserts = {};
   }
 
   fillDb() {
@@ -12,21 +13,48 @@ class Filler {
   }
 
   clearDb() {
-    // TODO: заверное надо запросить в драйвере все корни или все докементы и удалить их
+    // TODO: наверное надо запросить в драйвере все корни или все докементы и удалить их
   }
 
   _convertSchemaPathToMold(schemaPath) {
     return schemaPath.replace(/.schema/, '');
   }
 
+  _prepareItem(itemSchema) {
+    const container = {};
+
+    // TODO: поддержка других типов - collection и тд
+    if (itemSchema.type == 'document' || itemSchema.type == 'container') {
+      _.each(itemSchema.schema, (value, name) => {
+        if (_.isFunction(value.dev)) {
+          container[name] = value.dev();
+        }
+        else if (_.isString(value.dev) || _.isNumber(value.dev)) {
+          container[name] = value.dev;
+        }
+        // TODO: поддержка вложенного контейнера - надо рекурсивно
+      })
+    }
+
+    console.log(5555, container)
+
+    return container;
+   }
+
   _fillCollection(schemaPath, repeats, itemSchema) {
     const moldPath = this._convertSchemaPathToMold(schemaPath);
-    console.log(222222, moldPath, repeats, itemSchema)
+    console.log(222222, moldPath, repeats, itemSchema);
+
+    for (let i=0; i < repeats; i++) {
+      this.mold.child(moldPath).create(this._prepareItem(itemSchema));
+    }
   }
 
-  _fillPrimitive(schemaPath, value) {
+  _fillContainer(schemaPath, value) {
+
     const moldPath = this._convertSchemaPathToMold(schemaPath);
     console.log(1111111, moldPath, value)
+    //this.mold.children(moldPath).update({});
   }
 
   _schemaRecursuveAdd(currentPath, currentValue) {
@@ -34,14 +62,15 @@ class Filler {
     _.each(currentValue, (item, name) => {
       const subPath = _.trimStart(`${currentPath}.${name}`, '.');
       if (item.dev) {
+        // TODO: надо наверное собрать все примитивы и установить разом
         if (_.isFunction(item.dev)) {
-          this._fillPrimitive(subPath, item.dev());
+          this._fillContainer(subPath, item.dev());
         }
         else if (_.isString(item.dev) || _.isNumber(item.dev)) {
-          this._fillPrimitive(subPath, item.dev);
+          this._fillContainer(subPath, item.dev);
         }
         else if (_.isPlainObject(item.dev) && _.isNumber(item.dev.repeat)) {
-          this._fillCollection(subPath, item.dev, item.item);
+          this._fillCollection(subPath, item.dev.repeat, item.item);
         }
       }
       this._schemaRecursuveAdd(subPath, item);
